792caa7450f9c8e01a7b5a34fafca06f696efd7c
Aaron Foucheaux <afoucheaux@gmail.com>
2021-07-06 16:49:48 -0500

    component ProfileLanding basic setup

---
+++ src/components/ProfileLanding/ProfileLanding.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProfileLanding.css';
+
+const ProfileLanding = () => {
+  return (
+    <section className='profileLandingLayout'>
+      <article className='profileLandingCard'>
+        <h1 className='name'>Aaron Foucheaux</h1>
+        <h2> className='title'>Software Developer</h2>
+        <p className='kindOfDev'>Need to come up with statement.</p>
+      <article>
+    </section>
+  )
+}
