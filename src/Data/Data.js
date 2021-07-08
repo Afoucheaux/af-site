56e8974382f5e3a0b9571fe04947e730597034eb
Aaron Foucheaux <afoucheaux@gmail.com>
2021-07-08 09:34:21 -0500

    Gallery Time data added.

---
+++ src/Data/Data.js
@@ -0,0 +1,10 @@
+export const workData = [
+  {
+    id: 1,
+    title: 'Gallery Time',
+    techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'PWA'],
+    desc: 'This application allows users to view a list of galleries based on their geo-location, see details on individual gallery and save then to their favorites.',
+    mainImg: '../image/GallaryTime/Gallery1.png',
+    allImg: []
+  }
+]
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Work.css';
+import { workData } from '../../Data/Data.js'
 
 const Work = () => {
+  console.log(workData[0].mainImg)
   return (
     <section id='work' className='workLayout'>
       <p>Section One</p>
+      <img src={workData[0].mainImg} />
     </section>
   )
 }
