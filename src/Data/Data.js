0b426808d12acf5b34771dcb510e8f1cf133e479
Aaron Foucheaux <afoucheaux@gmail.com>
2021-07-16 11:08:26 -0500

    update to data file

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -3,45 +3,60 @@ export const workData = [
     id: 1,
     title: 'Gallery Time',
     techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'PWA', 'Cypress'],
-    desc: 'This application allows users to view a list of galleries based on their geo-location, see details on individual gallery and save then to their favorites.',
+    repo: 'https://github.com/Afoucheaux/gallery-time',
+    deployed: 'https://gallery-time.herokuapp.com/',
+    desc: 'Allows users to view a list of galleries based on their location and save them to their favorites.',
+    advDesc: 'This project utilizes Create React App and PWA technologies to build an application which allows users to view a list galleries based on their geo-location or a city of there selection. Then see details on individual gallery and save them to there favorites.',
     mainImg: 'https://i.postimg.cc/rphyjLxP/Gallery1.png',
     allImg: ['https://i.postimg.cc/rphyjLxP/Gallery1.png', 'https://i.postimg.cc/jq6f6DCg/Gallery2.png', 'https://i.postimg.cc/gkXWFZR4/Gallery3.png', 'https://i.postimg.cc/cJmqMMy1/Gallery4.png', 'https://i.postimg.cc/FFfrm8Xj/Gallery5.png'],
-    gifs: []
+    gifs: ['https://media.giphy.com/media/t17g16S5Oc48f7jzt3/giphy.gif', 'https://media.giphy.com/media/Vdmty5cR606UHXWbOc/giphy.gif', 'https://media.giphy.com/media/I1dkNGUzMxbmS8fi5x/giphy.gif']
   },
   {
     id: 2,
     title: 'Inu-Am-I',
     techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress'],
+    repo: 'https://github.com/Afoucheaux/inu-am-i',
+    deployed: 'https://inu-am-i.herokuapp.com/',
     desc: 'Fun application that alowes users to play a simply matching game.',
+    advDesc: 'This project is a solo project that utilizes Create React App, to build an application. The Application allows users to match Shiba Inu"s images from a mix of dogs and cats. A user page and functionality to view past rounds.',
     mainImg: 'https://i.postimg.cc/rFCvLNs1/InuOne.png',
     allImg: ['https://i.postimg.cc/rFCvLNs1/InuOne.png', 'https://i.postimg.cc/t4dRw6Qw/InuTwo.png', 'https://i.postimg.cc/50rfLqLY/InuThree.png', 'https://i.postimg.cc/2yv35XM8/InuFour.png'],
-    gifs: []
+    gifs: ['https://media.giphy.com/media/Te1QItBni7ZVuubRvE/giphy.gif', 'https://media.giphy.com/media/2JvC60dgJIzVBks5s9/giphy.gif', 'https://media.giphy.com/media/E8CJn6uKgiaQdq1Vbc/giphy.gif']
   },
   {
     id: 3,
     title: 'Rancid Tomatillos',
     techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress'],
-    desc: 'Move search app.',
+    repo: 'https://github.com/Afoucheaux/rancid_tomatillos',
+    deployed: 'https://limitless-escarpment-37745.herokuapp.com/',
+    desc: 'Move search application demo.',
+    advDesc: 'This project utilizes Create React App to build a movie application which allows users to view a list of movies, search by title, and find more information about each movie including a trailer.',
     mainImg: 'https://i.postimg.cc/Pqx7kGvZ/Rancid1.png',
     allImg: ['https://i.postimg.cc/Pqx7kGvZ/Rancid1.png', 'https://i.postimg.cc/pVZ1yXQ8/Rancid2.png', 'https://i.postimg.cc/pLZS7fkm/Rancid3.png'],
-    gifs: []
+    gifs: ['https://media.giphy.com/media/aitKojS2Tgzh2lIgM9/giphy.gif', 'https://media.giphy.com/media/nf3cgXfjJS9J7aPjQ3/giphy.gif', 'https://media.giphy.com/media/Bs4aPIEWSERpRAzen4/giphy.gif']
   },
   {
     id: 4,
     title: 'Overlook Inn',
     techUsed: ['JavaScript', 'HTML5', 'Sass', 'Mocha/Chai'],
-    desc: 'Hotel Booking app',
+    repo: 'https://github.com/Afoucheaux/overlook',
+    deployed: '',
+    desc: 'Hotel Booking application demo',
+    advDesc: 'This project utilize JavaScript to build a hotle booking app.'
     mainImg: 'https://i.postimg.cc/6QhBM44c/Overlook1.png',
     allImg: ['https://i.postimg.cc/6QhBM44c/Overlook1.png', 'https://i.postimg.cc/Yqg019Rn/Overlook2.png', 'https://i.postimg.cc/bw7RmsfZ/Overlook3.png', 'https://i.postimg.cc/Y2vzB62v/Overlook4.png'],
-    gifs: []
+    gifs: ['https://media.giphy.com/media/2F4oUAYpBc0eFTr93x/giphy.gif', 'https://media.giphy.com/media/3HOzvJBrPv1cTxLfKm/giphy.gif']
   },
   {
     id: 5,
     title: 'Job Finder',
-    techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress'],
-    desc: 'Job Finding app',
+    techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress', 'Travis CI'],
+    repo: 'https://github.com/Afoucheaux/FrontEnd',
+    deployed: 'https://jobfinderfe.herokuapp.com/',
+    desc: 'Job Finding application',
+    desc: 'This application allows a new user create a profile and login. The user then can search and filter jobs based on city and position. Then they have the ability to save jobs and go to a user profile. In the user profile they are able to view additional details about the jobs saved and link to apply.',
     mainImg: 'https://i.postimg.cc/8khvdtZg/Job1.png',
     allImg: ['https://i.postimg.cc/8khvdtZg/Job1.png', 'https://i.postimg.cc/65MyzSRg/Job2.png', 'https://i.postimg.cc/ydCdkz4g/Job3.png', 'https://i.postimg.cc/C10hwHkb/Job4.png', 'https://i.postimg.cc/j25rj85n/Job5.png', 'https://i.postimg.cc/RV2zMLzq/Job6.png'],
-    gifs: []
+    gifs: ['https://media.giphy.com/media/um4BxNVkVa4Q0Kazoy/giphy.gif', 'https://media.giphy.com/media/jK5LWZ993RSdKAvVYz/giphy.gif', 'https://media.giphy.com/media/JfXU1IHczQfMk2mKnt/giphy.gif', 'https://media.giphy.com/media/bvI9xSflR6EJEEK0Wc/giphy.gif']
   }
 ]
