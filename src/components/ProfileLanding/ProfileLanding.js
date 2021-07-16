48042a570a8f375ba99680d43d35e31b981d749f
Aaron Foucheaux <afoucheaux@gmail.com>
2021-07-16 12:36:50 -0500

    Update styling

--- src/components/ProfileLanding/ProfileLanding.js
+++ src/components/ProfileLanding/ProfileLanding.js
@@ -8,8 +8,10 @@ const ProfileLanding = () => {
         <h1 className='name'>Aaron Foucheaux</h1>
         <h2 className='title'>Software Developer</h2>
         <p className='kindOfDev'>Need to come up with a statement.</p>
-        <a href='https://www.linkedin.com/in/aaron-foucheaux-891626207/'><img src='https://i.postimg.cc/FRYhc0Yr/icons8-linkedin-circled-50.png' /></a>
-        <a href='https://github.com/Afoucheaux'><img src='https://i.postimg.cc/W4Hv2tXQ/icons8-github-50.png' /></a>
+        <div className='linkStylingBox'>
+          <a href='https://www.linkedin.com/in/aaron-foucheaux-891626207/' className='linkText' target='_blank'><img className='linkImg' src='https://i.postimg.cc/FRYhc0Yr/icons8-linkedin-circled-50.png' alt='AF linkedIn link'/>LinkedIn</a>
+          <a href='https://github.com/Afoucheaux' className='linkText' target='_blank'><img className='linkImg' src='https://i.postimg.cc/W4Hv2tXQ/icons8-github-50.png' alt='AF GitHub link'/>GitHub</a>
+        </div>
       </article>
     </section>
   )
