817eb8fb679d618cf2d884bae39a22ffd23b169b
Aaron Foucheaux <afoucheaux@gmail.com>
2021-07-29 15:37:25 -0500

    feature DetailCard basic functionality complete.

---
+++ src/components/DetailCard/DetailCard.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import './DetailCard.css';
+
+const DetailCard = ({title, advDesc, allImg, gifs, repo, deployed}) => {
+  const [imageDisplay, setImageDisplay] = useState([])
+  const [gifDisplay, setGifDisplay] = useState([])
+
+  useEffect(() => {
+    if (allImg !== undefined) {
+      setImageDisplay(allImg)
+    } else {
+      setImageDisplay([])
+    }
+  }, [allImg])
+
+  useEffect(() => {
+    if (gifs !== undefined) {
+      setGifDisplay(gifs)
+    } else {
+      setGifDisplay([])
+    }
+  }, [gifs])
+
+  const displayImg = imageDisplay.map((img, i) => {
+    return (
+      <img src={img} alt={`${title} image ${i}`} className='DetailImage'/>
+    )
+  })
+
+  const displayGif = gifDisplay.map((gif, i) => {
+    return (
+      <img src={gif} alt={`${title} gif ${i}`} className='DetailGif'/>
+    )
+  })
+
+  return (
+    <article className='detailLayout'>
+      <h1 className='detailTitle'>{title}</h1>
+      <p className='detailedDesc'>{advDesc}</p>
+      {displayImg}
+      {displayGif}
+      <a href={repo} className='linkRepo' target='_blank'>Repo</a>
+      <a href={deployed} className='linkDeployed' target='_blank'>Site</a>
+    </article >
+  )
+}
+
+export default DetailCard;
--- src/containers/DetailView/DetailView.js
+++ src/containers/DetailView/DetailView.js
@@ -1,13 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import Header from '../../components/Header/Header';
+import './DetailView.css';
+import { workData } from '../../Data/Data.js';
+import DetailCard from '../../components/DetailCard/DetailCard.js';
 
 const DetailView = ({ id }) => {
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    findProject()
+  }, [])
+
+  const findProject = () => {
+    const newProject = workData.find(proj => proj.id == id);
+    setProject(newProject)
+  }
+
   return (
     <main>
       <Header />
-      <p>{id}</p>
+      {project && <DetailCard title={project.title} advDesc={project.advDesc} allImg={project.allImg} gifs={project.gifs} repo={project.repo} deployed={project.deployed} />}
     </main>
   )
 }
 
-export default DetailView
+export default DetailView;
