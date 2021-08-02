b1cecf12dae76c0c10bc680eefcba1744e48ef13
Aaron Foucheaux <afoucheaux@gmail.com>
2021-08-02 09:44:40 -0500

    Updated data for advDesc.

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -6,7 +6,7 @@ export const workData = [
     repo: 'https://github.com/Afoucheaux/gallery-time',
     deployed: 'https://gallery-time.herokuapp.com/',
     desc: 'Allows users to view a list of galleries based on their location and save them to their favorites.',
-    advDesc: 'This project utilizes Create React App and PWA technologies to build an application which allows users to view a list galleries based on their geo-location or a city of there selection. Then see details on individual gallery and save them to there favorites.',
+    advDesc: 'This project is a Progressive Web Application. That utilizes React with hooks on the FrontEnd along with Cypress end to end testing. The app is a mobile first design that allows users to view a list of galleries based on their geo-location or a city of their selection. Then see details on individual galleries including images, hours and ratings. Then allows the user to save them to their favorites. It also has limited offline capability based on the utilization of PWA technologies of a service worker and the cache. Work on this project is ongoing on a limited basis.',
     mainImg: 'https://i.postimg.cc/rphyjLxP/Gallery1.png',
     allImg: ['https://i.postimg.cc/rphyjLxP/Gallery1.png', 'https://i.postimg.cc/jq6f6DCg/Gallery2.png', 'https://i.postimg.cc/gkXWFZR4/Gallery3.png', 'https://i.postimg.cc/cJmqMMy1/Gallery4.png', 'https://i.postimg.cc/FFfrm8Xj/Gallery5.png'],
     gifs: ['https://media.giphy.com/media/t17g16S5Oc48f7jzt3/giphy.gif', 'https://media.giphy.com/media/Vdmty5cR606UHXWbOc/giphy.gif', 'https://media.giphy.com/media/I1dkNGUzMxbmS8fi5x/giphy.gif']
@@ -18,7 +18,7 @@ export const workData = [
     repo: 'https://github.com/Afoucheaux/inu-am-i',
     deployed: 'https://inu-am-i.herokuapp.com/',
     desc: 'Fun application that alowes users to play a simply matching game.',
-    advDesc: 'This project is a solo project that utilizes Create React App, to build an application. The Application allows users to match Shiba Inu"s images from a mix of dogs and cats. A user page and functionality to view past rounds.',
+    advDesc: 'This project was a 5 day build that utilized React with hooks on the FrontEnd along with React Router and tested with Cypress to build this application. The app allows users to match Shiba Inu"s images from a mix of dogs and cats. User views and functionality include a login screen, game board and past results. Work on this project has stopped.',
     mainImg: 'https://i.postimg.cc/rFCvLNs1/InuOne.png',
     allImg: ['https://i.postimg.cc/rFCvLNs1/InuOne.png', 'https://i.postimg.cc/t4dRw6Qw/InuTwo.png', 'https://i.postimg.cc/50rfLqLY/InuThree.png', 'https://i.postimg.cc/2yv35XM8/InuFour.png'],
     gifs: ['https://media.giphy.com/media/Te1QItBni7ZVuubRvE/giphy.gif', 'https://media.giphy.com/media/2JvC60dgJIzVBks5s9/giphy.gif', 'https://media.giphy.com/media/E8CJn6uKgiaQdq1Vbc/giphy.gif']
@@ -29,8 +29,8 @@ export const workData = [
     techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress'],
     repo: 'https://github.com/Afoucheaux/rancid_tomatillos',
     deployed: 'https://limitless-escarpment-37745.herokuapp.com/',
-    desc: 'Move search application demo.',
-    advDesc: 'This project utilizes Create React App to build a movie application which allows users to view a list of movies, search by title, and find more information about each movie including a trailer.',
+    desc: 'Move search application FrontEnd demo.',
+    advDesc: 'This project utilizes Create React App with React Router and tested with Cypress on this build. The application allows users to view a list of movies, search by title, and find more information about each movie including a trailer. Work on this project has stopped.',
     mainImg: 'https://i.postimg.cc/Pqx7kGvZ/Rancid1.png',
     allImg: ['https://i.postimg.cc/Pqx7kGvZ/Rancid1.png', 'https://i.postimg.cc/pVZ1yXQ8/Rancid2.png', 'https://i.postimg.cc/pLZS7fkm/Rancid3.png'],
     gifs: ['https://media.giphy.com/media/aitKojS2Tgzh2lIgM9/giphy.gif', 'https://media.giphy.com/media/nf3cgXfjJS9J7aPjQ3/giphy.gif', 'https://media.giphy.com/media/Bs4aPIEWSERpRAzen4/giphy.gif']
@@ -41,8 +41,8 @@ export const workData = [
     techUsed: ['JavaScript', 'HTML5', 'Sass', 'Mocha/Chai'],
     repo: 'https://github.com/Afoucheaux/overlook',
     deployed: '',
-    desc: 'Hotel Booking application demo',
-    advDesc: 'This project utilize JavaScript to build a hotle booking app.',
+    desc: 'Hotel Booking FrontEnd demo',
+    advDesc: 'This project utilizes JavaScript, HTML and a CSS preprocessor to build a hotel booking app. Users have functionality to see past bookings and have a total spend. The ability to filter and check availability of rooms. Then get a cost per room and book if interested. This was a 5 day build and all work on this project has stopped.',
     mainImg: 'https://i.postimg.cc/6QhBM44c/Overlook1.png',
     allImg: ['https://i.postimg.cc/6QhBM44c/Overlook1.png', 'https://i.postimg.cc/Yqg019Rn/Overlook2.png', 'https://i.postimg.cc/bw7RmsfZ/Overlook3.png', 'https://i.postimg.cc/Y2vzB62v/Overlook4.png'],
     gifs: ['https://media.giphy.com/media/2F4oUAYpBc0eFTr93x/giphy.gif', 'https://media.giphy.com/media/3HOzvJBrPv1cTxLfKm/giphy.gif']
@@ -50,11 +50,11 @@ export const workData = [
   {
     id: 5,
     title: 'Job Finder',
-    techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress', 'Travis CI'],
+    techUsed: ['JavaScript', 'React', 'HTML5', 'CSS', 'Cypress', 'Travis CI', 'TypeScript'],
     repo: 'https://github.com/Afoucheaux/FrontEnd',
     deployed: 'https://jobfinderfe.herokuapp.com/',
     desc: 'Job Finding application',
-    advDesc: 'This application allows a new user create a profile and login. The user then can search and filter jobs based on city and position. Then they have the ability to save jobs and go to a user profile. In the user profile they are able to view additional details about the jobs saved and link to apply.',
+    advDesc: "This application allows a new user to create a profile and login. The user then can search and filter tech jobs based on city and position. Then they have the ability to save jobs to their profile and go to a user's save jobs. In the user profile they are able to view additional details about the jobs saved and link to apply. This project is a React application that originally utilized JS and was later updated to TypeScript. Context API utilized global state management to achieve light/dark mode and the app was tested with Cypress. This was an integrated project with the BE team and all work on this project has stopped.",
     mainImg: 'https://i.postimg.cc/8khvdtZg/Job1.png',
     allImg: ['https://i.postimg.cc/8khvdtZg/Job1.png', 'https://i.postimg.cc/65MyzSRg/Job2.png', 'https://i.postimg.cc/ydCdkz4g/Job3.png', 'https://i.postimg.cc/C10hwHkb/Job4.png', 'https://i.postimg.cc/j25rj85n/Job5.png', 'https://i.postimg.cc/RV2zMLzq/Job6.png'],
     gifs: ['https://media.giphy.com/media/um4BxNVkVa4Q0Kazoy/giphy.gif', 'https://media.giphy.com/media/jK5LWZ993RSdKAvVYz/giphy.gif', 'https://media.giphy.com/media/JfXU1IHczQfMk2mKnt/giphy.gif', 'https://media.giphy.com/media/bvI9xSflR6EJEEK0Wc/giphy.gif']
