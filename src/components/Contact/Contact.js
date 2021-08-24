4b3fbef4081cd28685ea41a57e363c1635fbb71b
Aaron Foucheaux <afoucheaux@gmail.com>
2021-08-24 15:43:12 -0500

    Email me link added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,12 +6,13 @@ const Contact = () => {
     <section id='contact' className='contactLayout'>
       <article className='contactCard'>
         <p className=''>MY PICK HERE</p>
-        <p>Contact me with the following links or view my GitHub profile.</p>
+        <p>Contact me or view my GitHub profile at the following links.</p>
         <div className='linkStylingBox'>
           <a href='https://www.linkedin.com/in/aaron-foucheaux-891626207/' className='linkText' target='_blank'><img className='linkImg' src='https://i.postimg.cc/FRYhc0Yr/icons8-linkedin-circled-50.png' alt='AF linkedIn link'/>LinkedIn</a>
+          <a href='mailto: afoucheaux@gmail.com' className='linkText'><img className='linkImg linkImgEmail' src='https://img.icons8.com/ios-glyphs/30/000000/email.png' alt='AF email'/>Email</a>
           <a href='https://github.com/Afoucheaux' className='linkText' target='_blank'><img className='linkImg' src='https://i.postimg.cc/W4Hv2tXQ/icons8-github-50.png' alt='AF GitHub link'/>GitHub</a>
         </div>
-          <p className='thisSiteSent'>Click <a href='https://github.com/Afoucheaux/af-site' target='_blank' className='thisSiteLink'>HERE </a> to see the code for this site.</p>
+          <p className='thisSiteSent'>Click <a href='https://github.com/Afoucheaux/af-site' target='_blank' className='thisSiteLink'>HERE </a> to see the repo for this site.</p>
       </article>
     </section>
   )
